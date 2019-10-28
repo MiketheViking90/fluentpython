eb21fc2a2a9db9c7e9e592b1fca28dcfbe84791c
Jim Wen <jimwen@lyft.com>
2019-10-28 13:32:01 -0700

    Finish card_deck, start num types and vector

--- src/ch01/card_deck.py
+++ src/ch01/card_deck.py
@@ -3,6 +3,17 @@ from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
+suit_values = dict(
+        spades=3,
+        hearts=2,
+        diamonds=1,
+        clubs=0
+    )
+
+def rank_spades_high(card):
+    rank_val = FrenchDeck.ranks.index(card.rank)
+    return rank_val * len(suit_values) + suit_values[card.suit]
+
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades, diamonds, clubs, hearts'.split(", ")
@@ -17,11 +28,26 @@ class FrenchDeck:
     def __getitem__(self, pos):
         return self._cards[pos]
 
-beer_card = Card('7', 'clubs')
-print(beer_card)
+def random_choice_demo():
+    beer_card = Card('7', 'clubs')
+    print(beer_card)
 
-deck = FrenchDeck()
+    deck = FrenchDeck()
 
-print(choice(deck))
-print(choice(deck))
-print(choice(deck))
+    print(choice(deck))
+    print(choice(deck))
+    print(choice(deck))
+
+def getitem_demo():
+    deck = FrenchDeck()
+    for card in deck:
+        print(card)
+    for card in reversed(deck):
+        print(card)
+
+def sort_demo():
+    deck = FrenchDeck()
+    for card in sorted(deck, key=rank_spades_high):
+        print(card)
+
+sort_demo()
