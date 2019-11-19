5bcc942d8bde400373ec639d486e162c125f8652
Jim Wen <jimwen@lyft.com>
2019-11-18 17:30:11 -0800

    Dict add to list

--- src/ch03/dict_init.py
+++ src/ch03/dict_init.py
@@ -18,3 +18,10 @@ pprint(codes)
 
 codes_mixed = {country.upper(): code+4 for code, country in DIAL_CODES}
 pprint(codes_mixed)
+
+data = [("USA", 1), ("CHN", 17), ("USA", 4), ("RUS", 4), ("USA", 2), ("CHN", 8)]
+
+country_stats = {}
+for country, stat in data:
+    country_stats.setdefault(country, []).append(stat)
+pprint(country_stats)
