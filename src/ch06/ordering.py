8f04c57e6a6d47cd769eeabbaa1c3e5b1769773c
MiketheViking90 <siliwen@gmail.com>
2019-11-25 14:07:54 -0800

    add \n

--- src/ch06/ordering.py
+++ src/ch06/ordering.py
@@ -14,15 +14,16 @@ def large_cart_promotion(order: Order):
         return .07 * order.total()
     return 0
 
-
+def best_promotion(order):
+    return max(bulk_item_promotion(order), large_cart_promotion(order))
 
 cart = [LineItem('banana', 4, .5), LineItem('apple', 10, 1.5), LineItem('butter', 25, 7.4)]
-order1 = Order('joe', cart, bulk_item_promotion)
+order1 = Order('joe', cart, best_promotion)
 due1 = order1.due()
 total1 = order1.total()
 print(due1, total1)
 
-order2 = Order('joe', cart, large_cart_promotion)
+order2 = Order('joe', cart, best_promotion)
 due2 = order2.due()
 total2 = order2.total()
 print(due2, total2)
